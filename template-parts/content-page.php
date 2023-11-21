94937375596b83ac313f4356a42f88d6c6c71d78
Danish <danish.ehsan321@gmail.com>
2023-11-20 20:53:47 -0500

    Update page html template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,14 +9,12 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('l-content'); ?>>
-	<header class="entry-header">
+	<header class="l-content entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<?php wp_framework_post_thumbnail(); ?>
 
-	<div class="entry-content">
 		<?php
 		the_content();
 
@@ -27,7 +25,6 @@
 			)
 		);
 		?>
-	</div><!-- .entry-content -->
 
 	<?php if ( get_edit_post_link() ) : ?>
 		<footer class="entry-footer">
@@ -51,4 +48,3 @@
 			?>
 		</footer><!-- .entry-footer -->
 	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
